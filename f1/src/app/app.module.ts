8ed7538fa80183a9734ae506e3497bc2f3184415
Abrar Zahin <abrarzahin@yahoo.com>
2018-09-07 21:48:49 -0400

    4th

--- f1/src/app/app.module.ts
+++ f1/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule } from '@angular/material';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 import {RouterModule } from '@angular/router';
 import { MessagesComponent } from './messages.component';
 import { AppComponent } from './app.component';
@@ -12,6 +12,7 @@ import { NewMessageComponent } from './new-message.component';
 import { NavComponent} from './nav.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {HomeComponent} from './home.component';
+import {RegisterComponent} from './register.component';
 
 
 
@@ -27,11 +28,16 @@ var routes =[{
   path:'messages/:name',
   component: MessagesComponent
 }
+,
+{
+  path:'register',
+  component: RegisterComponent
+}
 ];
 
 @NgModule({
-  imports: [BrowserModule, HttpModule,RouterModule.forRoot(routes), FormsModule, BrowserAnimationsModule,MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule],
-  declarations: [AppComponent,NavComponent, MessagesComponent, NewMessageComponent,HomeComponent],
+  imports: [BrowserModule, HttpModule,RouterModule.forRoot(routes), FormsModule,ReactiveFormsModule, BrowserAnimationsModule,MatToolbarModule, MatButtonModule, MatCardModule, MatInputModule, MatSnackBarModule],
+  declarations: [AppComponent,NavComponent, MessagesComponent, NewMessageComponent,HomeComponent,RegisterComponent],
   bootstrap: [AppComponent],
   providers: [WebService]
 })
